f1eb2970e56cd3686870385b7d5e1d3ff2d23f5f
maxinyuan <984501240@qq.com>
2020-12-28 17:44:22 +0800

    树莓派智能网络小车

---
+++ src/main/java/com/mxy/car/Application.java
@@ -0,0 +1,13 @@
+package com.mxy.car;
+
+/**
+ * Created by IDEA.
+ *
+ * @author maxinyuan
+ * @date 2020/12/28
+ */
+public class Application {
+  public static void main(String[] args) {
+    System.out.println("只能小车");
+  }
+}
